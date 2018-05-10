5f1ee303d2cc18b1ce3004f71ae2cb0d07ceb7cb
chrisgoodell <cg53142@fastmail.com>
2018-05-10 11:12:35 -0400

    Update map centering

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
+const style = {
+  width: '100%',
+  height: '100%'
+}
+
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map 
+        google={this.props.google} 
+        zoom={14}
+        style={style}
+        initialCenter={{
+          lat: 33.7490,
+          lng: -84.3880
+        }}
+        center={{
+          lat: 33.7490,
+          lng: -84.3880
+        }}
+        onClick={this.onMapClicked}
+        >
  
         <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
